c2304663d885176bbf9927c1cf4698907aeda0e6
M3chD09 <m3chd09@protonmail.com>
2021-03-22 22:53:50 +0800

    comments on datautil

--- datautil/const.go
+++ datautil/const.go
@@ -1,22 +1,27 @@
+/*
+想给你写封信，却不知从何说起
+*/
 package datautil
 
-const MAX_PACKET_NUM = 5
+// 篇幅有限，也挡不住我的思念
+const NumMaxPacket = 5
 
 const (
-	_ = iota
-	ActModeSubscribe
-	ActModeSubscribeReturn
-	ActModeUnSubscribe
-	ActModeUnSubscribeReturn
-	ActModeRead
-	ActModeReadReturn
-	ActModeWrite
-	ActModeWriteReturn
+	_                        = iota
+	ActModeSubscribe         // 想让你一直联系
+	ActModeSubscribeReturn   // 期待你认真回应
+	ActModeUnSubscribe       // 却担心打扰到你
+	ActModeUnSubscribeReturn // 有回复就够了呢
+	ActModeRead              // 你在想什么
+	ActModeReadReturn        // 能告诉我吗
+	ActModeWrite             // 希望改变你的心意
+	ActModeWriteReturn       // 传达到了吗
 )
 
+// 你现在在哪里呢
 const (
-	_ = iota
-	Board1
-	Board2
+	_      = iota
+	Board1 // 是近在咫尺
+	Board2 // 还是远在天边
 	Board3
 )
--- datautil/datautil.go
+++ datautil/datautil.go
@@ -1,3 +1,4 @@
+/**/
 package datautil
 
 import (
@@ -6,44 +7,68 @@ import (
 	"github.com/scutrobotlab/asuwave/variable"
 )
 
+// 把我的思念，化作一串珍珠送给你
 func MakeCmd(act uint8, v *variable.T) []byte {
+
+	// 拾起一颗颗珍珠
 	data := make([]byte, 16)
+
+	// 刻上你所在的城市和我的思念
 	data[0] = byte(v.Board)
 	data[1] = act
 	data[2] = byte(variable.TypeLen[v.Type])
 	copy(data[3:7], variable.AnyToBytes(v.Addr))
+
+	// 若是心意能够改变
 	if act == ActModeWrite {
+		// 便也一并传递给你
 		copy(data[7:15], variable.SpecToBytes(v.Type, v.Data))
 	}
+
+	// 终于完成了
 	data[15] = '\n'
+
+	// 传达给你吧
 	return data
 }
 
+// 从破碎的碎片中，渴望拼凑出你的痕迹
 func FindValidPart(data []byte) (int, int) {
 	n := len(data)
+
+	// 我们的羁绊，岂是三言两语能道尽的
 	if n < 20 {
 		return 0, 0
 	}
 
+	// 从头开始吧，画一扇窗
 	l := 0
 	r := 19
 
+	// 苦苦寻找你的痕迹
 	for r < n && (data[l] != Board1 || data[l+1] != ActModeSubscribeReturn || data[r] != '\n') {
 		l++
 		r++
 	}
 
+	// 也许无功而返
 	if r == n {
 		return 0, 0
 	}
 
+	// 也许收获满满
 	r = n - (n-r)%20
 	return l, r + 1
 }
 
+// 你的痕迹，我找到了
 func MakeChartPack(x *variable.ListChartT, y *variable.ListT, data []byte) {
 	var chartData variable.ToChartT
+
+	// 一段一段的回忆被整理出来
 	for i := 0; i < len(data)/20; i++ {
+
+		// 你的城市、心跳和住址
 		chartData.Board = data[i*20]
 		chartData.Tick = variable.BytesToUint32(data[i*20+15 : i*20+19])
 		addr := variable.BytesToUint32(data[i*20+3 : i*20+7])
