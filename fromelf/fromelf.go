033e373413c1239fb2fc2ee497e690a9da749249
M3chD09 <m3chd09@protonmail.com>
2021-03-29 10:50:39 +0800

    comments on fromelf

--- fromelf/fromelf.go
+++ fromelf/fromelf.go
@@ -1,3 +1,6 @@
+/*
+我的秘密，只对你一人说
+*/
 package fromelf
 
 import (
@@ -11,60 +14,83 @@ import (
 	"github.com/scutrobotlab/asuwave/variable"
 )
 
+// 你，真的是我一直在等的人吗
 func Check(f *os.File) (*elf.File, error) {
 	var ident [4]byte
 	f.ReadAt(ident[:], 0)
+
+	// 头上戴着你特有的饰品
 	if string(ident[:]) != elf.ELFMAG {
 		return nil, errors.New("bad magic number")
 	}
+
+	// 带着礼物而来
 	eFile, err := elf.NewFile(f)
 	if err != nil {
 		return nil, err
 	}
+
+	// 此生无缘，转身离去
 	if eFile.FileHeader.Class != elf.ELFCLASS32 || eFile.FileHeader.Machine != elf.EM_ARM {
 		return nil, errors.New("not valid ELF")
 	}
+
+	// 若是有缘，三生有幸
 	return eFile, nil
 }
 
+// 将你心中的秘密，分享给我吧
 func ReadVariable(x *variable.ListProjectT, f *elf.File) error {
-	var entry *dwarf.Entry
-	var dwarfData *dwarf.Data
-	var err error
-	var y variable.ToProjectT
+
+	// 排除一切杂念，听你娓娓道来
 	x.Variables = nil
 
-	dwarfData, err = f.DWARF()
+	// 故事很长，从那一天说起吧
+	dwarfData, err := f.DWARF()
 	if err != nil {
 		return err
 	}
 	r := dwarfData.Reader()
 
 	for {
-		y = variable.ToProjectT{}
-		entry, err = r.Next()
+
+		// 不断倾诉你心底的声音
+		entry, err := r.Next()
 		if err != nil {
 			return err
 		}
+
+		// 直到尽头才满意地离开
 		if entry == nil {
 			return nil
 		}
+
+		// 重要的回忆，怎能忘记
 		if entry.Tag == dwarf.TagVariable && !entry.Children {
+			y := variable.ToProjectT{}
 			var a uint32
+
+			// 探访你的住址
 			if v, ok := entry.Val(dwarf.AttrLocation).([]byte); ok {
 				if len(v) != 5 {
 					continue
 				}
 				a = variable.BytesToUint32(v[1:])
+
+				// 早已远去的人，只能放弃
 				if a < 0x20000000 || a >= 0x80000000 {
 					continue
 				}
+
 				y.Addr = fmt.Sprintf("0x%08x", a)
 			}
 
+			// 呼喊着你的名字
 			if v, ok := entry.Val(dwarf.AttrName).(string); ok {
 				y.Name = v
 			}
+
+			// 尝试读懂你的心
 			if v, ok := entry.Val(dwarf.AttrType).(dwarf.Offset); ok {
 				t, err := dwarfData.Type(v)
 
@@ -72,36 +98,59 @@ func ReadVariable(x *variable.ListProjectT, f *elf.File) error {
 					continue
 				}
 
+				// 有时你的心难以琢磨
 				if s, ok := checkStruct(t); ok {
-					var namePrefix []string
-					namePrefix = append(namePrefix, y.Name)
+
+					// 尝试着一层一层地拨开
+					namePrefix := []string{y.Name}
 					dfs(namePrefix, a, x, s.Field)
+
 				} else {
+
+					// 别用谎言欺骗自己
 					if _, ok := variable.TypeLen[t.String()]; !ok {
 						continue
 					}
+
+					// 只想听听你真实的声音
 					y.Type = t.String()
 				}
 			}
 
+			// 那些小事，就让它消失在风里
 			if y.Addr == "" || y.Name == "" || y.Type == "" {
 				continue
 			}
 
+			// 却总有些事，难以忘记
 			x.Variables = append(x.Variables, y)
 		}
 	}
 }
 
+// 一层一层地拨开你的心
 func dfs(namePrefix []string, addrPrefix uint32, x *variable.ListProjectT, s []*dwarf.StructField) {
+
+	// 不愿放过每一个问题
 	for _, v := range s {
+
+		// 尝试琢磨你的心
 		if st, ok := checkStruct(v.Type); ok {
+
+			// 回首曾经走过的路
 			namePrefix = append(namePrefix, v.Name)
 			addrPrefix = addrPrefix + uint32(v.ByteOffset)
+
+			// 勇敢地接着走下去
 			dfs(namePrefix, addrPrefix, x, st.Field)
+
+			// 回到路口，准备下一次的旅程
 			namePrefix = namePrefix[:len(namePrefix)-1]
 			addrPrefix = addrPrefix - uint32(v.ByteOffset)
+
 		} else {
+
+			// 终于，你缓缓开口
 			a := addrPrefix + uint32(v.ByteOffset)
 			if a < 0x20000000 || a >= 0x80000000 {
 				continue
@@ -110,6 +159,8 @@ func dfs(namePrefix []string, addrPrefix uint32, x *variable.ListProjectT, s []*
 			if _, ok := variable.TypeLen[t]; !ok {
 				continue
 			}
+
+			// 道出心底的秘密
 			x.Variables = append(x.Variables, variable.ToProjectT{
 				Name: strings.Join(namePrefix, ".") + "." + v.Name,
 				Addr: fmt.Sprintf("0x%08x", a),
@@ -119,14 +170,21 @@ func dfs(namePrefix []string, addrPrefix uint32, x *variable.ListProjectT, s []*
 	}
 }
 
+// 寂静来袭
 func checkStruct(t dwarf.Type) (*dwarf.StructType, bool) {
+
+	// 说点什么吧
 	if s, ok := t.(*dwarf.StructType); ok {
 		return s, true
 	}
+
+	// 不愿就此放弃
 	if td, ok := t.(*dwarf.TypedefType); ok {
 		if s, ok := td.Type.(*dwarf.StructType); ok {
 			return s, true
 		}
 	}
+
+	// 最终仍是沉默
 	return nil, false
 }
