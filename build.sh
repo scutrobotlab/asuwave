38b02c687cf61c9f546f3a68e8213852a8658357
M3chD09 <m3chd09@protonmail.com>
2021-06-04 09:02:51 +0800

    add build for arm64

--- build.sh
+++ build.sh
@@ -13,13 +13,23 @@ fi
 
 build_prefix="asuwave_"
 os_list=("linux" "darwin" "windows")
-build_suffix=("" "" ".exe")
+arch_list=("amd64" "arm64")
 flags="-w -s -X 'main.githash=$(git describe --tags --long)' -X 'main.buildtime=$(date)' -X 'main.goversion=$(go version)'"
 
-for ((i = 0 ; i < 3 ; i++)); do
-    file=$build_dir/$build_prefix${os_list[$i]}
-    out=$file${build_suffix[$i]}
-    CGO_ENABLED=0 GOOS=${os_list[$i]} GOARCH=amd64 go build -tags release -ldflags="$flags" -o $out
-    upx -q -9 $out
-    zip -j -9 $file.zip $out
+for os in ${os_list[@]}; do
+    for arch in ${arch_list[@]}; do
+        suffix=""
+        if [ "$os" == "windows" ] ; then
+            if [ "$arch" == "arm64" ]; then
+                continue
+            else
+                suffix=".exe"
+            fi
+        fi
+        file=$build_dir/$build_prefix${os}_${arch}
+        out=$file$suffix
+        CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} go build -tags release -ldflags="$flags" -o $out
+        upx -q -9 $out > /dev/null
+        zip -j -9 $file.zip $out
+    done
 done
