724509ea8b62dae3ac708de2cbb81d98a434de01
M3chD09 <m3chd09@protonmail.com>
2021-06-04 10:53:07 +0800

    log mv to dir

--- logger/logger.go
+++ logger/logger.go
@@ -10,11 +10,19 @@ import (
 var Log *log.Logger
 
 func init() {
-	logpath := "asuwave-" + strconv.FormatInt(time.Now().Unix(), 10) + ".log"
+	if _, err := os.Stat("log"); os.IsNotExist(err) {
+		err := os.Mkdir("log", 0755)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	logpath := "log/asuwave-" + strconv.FormatInt(time.Now().Unix(), 10) + ".log"
 	file, err := os.Create(logpath)
 	if err != nil {
 		panic(err)
 	}
+
 	Log = log.New(file, "", log.LstdFlags|log.Lshortfile)
 	Log.Println("LogFile : " + logpath)
 }
