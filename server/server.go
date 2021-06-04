578849ea532e8fd2e489e0a4f3d8f30e225ca57d
M3chD09 <m3chd09@protonmail.com>
2021-06-04 10:00:09 +0800

    show url

--- server/server.go
+++ server/server.go
@@ -2,20 +2,25 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/fs"
-	"log"
+	"net"
 	"net/http"
 	"strconv"
 
+	"github.com/scutrobotlab/asuwave/logger"
 	"github.com/scutrobotlab/asuwave/option"
 	"github.com/scutrobotlab/asuwave/variable"
 )
 
 // Start server
 func Start(c chan string, fsys *fs.FS) {
-	host := "0.0.0.0:" + strconv.Itoa(option.Config.Port)
-	log.Println("Listen on " + host)
-	log.Println("Don't close this before you have done")
+	port := ":" + strconv.Itoa(option.Config.Port)
+
+	fmt.Println("asuwave running at:")
+	fmt.Println("- Local:   http://localhost" + port + "/")
+	fmt.Println("- Network: http://" + getLocalIP() + port + "/")
+	fmt.Println("Don't close this before you have done")
 
 	variableToReadCtrl := makeVariableCtrl(&variable.ToRead, true)
 	variableToModiCtrl := makeVariableCtrl(&variable.ToModi, false)
@@ -31,11 +36,26 @@ func Start(c chan string, fsys *fs.FS) {
 	http.HandleFunc("/variable_type", variableTypeCtrl)
 	http.HandleFunc("/option", optionCtrl)
 	http.HandleFunc("/ws", websocketCtrl)
-	log.Fatal(http.ListenAndServe(host, nil))
+	logger.Log.Fatal(http.ListenAndServe(port, nil))
 }
 
 func errorJson(s string) string {
 	j := struct{ Error string }{s}
 	b, _ := json.Marshal(j)
 	return string(b)
+}
+
+func getLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "127.0.0.1"
+	}
+
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+			return ipnet.IP.String()
+		}
+	}
+
+	return "127.0.0.1"
 }
